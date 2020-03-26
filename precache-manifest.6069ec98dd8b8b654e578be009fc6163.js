300ce0686531b4714f3f88349afaf901eb12741e
Prince Anuragi <princanurag07@gmail.com>
2020-03-26 16:14:36 +0530

    Updates

---
+++ precache-manifest.6069ec98dd8b8b654e578be009fc6163.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc3dfddd730168b9f23c0ae8e0507ea",
+    "url": "/prix0007/gh-pages-test.git/index.html"
+  },
+  {
+    "revision": "989f63dc4af4aa8263f9",
+    "url": "/prix0007/gh-pages-test.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eed7997836531ab5a6f7",
+    "url": "/prix0007/gh-pages-test.git/static/js/2.9fca7837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prix0007/gh-pages-test.git/static/js/2.9fca7837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989f63dc4af4aa8263f9",
+    "url": "/prix0007/gh-pages-test.git/static/js/main.90212fbf.chunk.js"
+  },
+  {
+    "revision": "78ec2f4007f9f789afb4",
+    "url": "/prix0007/gh-pages-test.git/static/js/runtime-main.38d73679.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prix0007/gh-pages-test.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
